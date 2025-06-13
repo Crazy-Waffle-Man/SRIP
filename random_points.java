5546d6cfd5d40458b527a471476ec00d662b7d0a
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2025-06-13 09:19:14 -0400

    random_points now houses a method

--- random_points.java
+++ random_points.java
@@ -1,27 +1,18 @@
 //Dylan Kavde
 //6/12/2025
 //Program will take points and rotate it such that a horizontal partition can be made to definitely take away that split of points. Random points will be created.
-import helpers.helperMethods;
 import java.util.Scanner;
 import java.util.Arrays;
 
 
-public class MyProgram
+public class random_points
 {
-    public static void main(String[] args)
+    public static double[][] generateRandomPoints(int n)
     {
-      /*switch (value) {
-          case (9) {
-              
-          }
-      }*/
-      
       //Initialize scanner
        Scanner scanner = new Scanner(System.in);
        
        //Find amount of points wanted to be tested
-       System.out.println("How many points does thou want");
-        int n = scanner.nextInt();
         double[][] points = new double[n][2];
         
         //Fills array points with random numbers
@@ -30,14 +21,6 @@ public class MyProgram
             points[i][j] =  10*Math.random();
             }
         }
-        
-        points = helperMethodes.randomPoints(n);
-        /*for (double[] point : points) {
-            System.out.println(Arrays.toString(point));
-        }*/
-       
-        
+        return points;
     }
-    
-    
 }
